375018d51497463867e54afc7798a36743fecd54
Gabriel Lutz <gabriel_lutz@hotmail.com>
2025-10-31 16:26:50 -0300

    feat(dtos-constancia): agrega DTOs de carga y respuesta de constancias

---
+++ PortalDGC/src/PortalDGC.Dtos/Constancia/ConstanciaResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDGC.Dtos.Constancia
+{
+    public class ConstanciaResponseDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Tipo { get; set; } = string.Empty;
+        public string Archivo { get; set; } = string.Empty;
+        public DateTime FechaSubida { get; set; }
+        public bool Validado { get; set; }
+    }
+}
