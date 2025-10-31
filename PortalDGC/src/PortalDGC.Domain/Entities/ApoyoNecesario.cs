5aad1d68a60aab37d43b6b76f31ec07c85dcbf86
Gabriel Lutz <gabriel_lutz@hotmail.com>
2025-10-31 15:22:41 -0300

    feat(domain): agrega apoyos al postulante (ApoyoNecesario y ApoyoSolicitado)

---
+++ PortalDGC/src/PortalDGC.Domain/Entities/ApoyoNecesario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDGC.Domain.Entities
+{
+    public class ApoyoNecesario
+    {
+        public int Id { get; set; }
+        public int LlamadoId { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public string Tipo { get; set; } = string.Empty;
+    }
+}
---
+++ PortalDGC/src/PortalDGC.Domain/Entities/ApoyoSolicitado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDGC.Domain.Entities
+{
+    public class ApoyoSolicitado
+    {
+        public int Id { get; set; }
+        public int InscripcionId { get; set; }
+        public int ApoyoId { get; set; }
+        public string? Justificacion { get; set; }
+    }
+}
