e5599c9fbd6483b8f8b96e983051b908ce12c111
Gabriel Lutz <gabriel_lutz@hotmail.com>
2025-10-31 16:40:05 -0300

    feat(dtos-llamado): agrega DTOs para llamados, departamentos, apoyos y requisitos

---
+++ PortalDGC/src/PortalDGC.Dtos/Llamado/RequisitoExcluyenteDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDGC.Dtos.Llamado
+{
+    public class RequisitoExcluyenteDto
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public string Tipo { get; set; } = string.Empty;
+        public bool Obligatorio { get; set; }
+    }
+}
