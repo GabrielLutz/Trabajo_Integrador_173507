5f28e5e6363004c7c428e63549998876361d2446
Gabriel Lutz <gabriel_lutz@hotmail.com>
2025-10-31 16:23:58 -0300

    feat(dtos-common): agrega DTOs comunes y estructura inicial de carpetas para el resto

---
+++ PortalDGC/src/PortalDGC.Dtos/Common/DepartamentoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDGC.Dtos.Common
+{
+    public class DepartamentoDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Codigo { get; set; } = string.Empty;
+    }
+}
