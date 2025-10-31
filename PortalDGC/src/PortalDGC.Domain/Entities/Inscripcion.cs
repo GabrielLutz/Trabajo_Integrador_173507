f8eea12109e4977d1275031f14ad1e6a9ab0eb86
Gabriel Lutz <gabriel_lutz@hotmail.com>
2025-10-31 15:25:28 -0300

    feat(domain): agrega entidad Inscripcion

---
+++ PortalDGC/src/PortalDGC.Domain/Entities/Inscripcion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDGC.Domain.Entities
+{
+    public class Inscripcion
+    {
+        public int Id { get; set; }
+        public int PostulanteId { get; set; }
+        public int LlamadoId { get; set; }
+        public int DepartamentoId { get; set; }
+        public DateTime FechaInscripcion { get; set; }
+        public string Estado { get; set; } = string.Empty;
+        public decimal PuntajeTotal { get; set; }
+        public int? PosicionOrdenamiento { get; set; }
+    }
+}
