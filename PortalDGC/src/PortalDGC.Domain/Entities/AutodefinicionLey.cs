4afffc79f76b3f800d9da4ca3870ced8712d6d51
Gabriel Lutz <gabriel_lutz@hotmail.com>
2025-10-31 15:11:36 -0300

    feat(domain): agrega entidades de postulante (Postulante, AutodefinicionLey, Constancia)

---
+++ PortalDGC/src/PortalDGC.Domain/Entities/AutodefinicionLey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDGC.Domain.Entities
+{
+    public class AutodefinicionLey
+    {
+        public int Id { get; set; }
+        public int InscripcionId { get; set; }
+        public bool EsAfrodescendiente { get; set; }
+        public bool EsTrans { get; set; }
+        public bool TieneDiscapacidad { get; set; }
+    }
+}
