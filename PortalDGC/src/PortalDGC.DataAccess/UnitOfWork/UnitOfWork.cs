8ac1ceec28debaac214c710342c58f7bf1750ac6
Gabriel Lutz <gabriel_lutz@hotmail.com>
2025-11-02 16:45:30 -0300

    feat(data): integra repositorios en UnitOfWork

--- PortalDGC/src/PortalDGC.DataAccess/UnitOfWork/UnitOfWork.cs
+++ PortalDGC/src/PortalDGC.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -1,4 +1,7 @@
-﻿using PortalDGC.DataAccess.Interfaces;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using PortalDGC.DataAccess.Data;
+using PortalDGC.DataAccess.Interfaces;
+using PortalDGC.DataAccess.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,53 +12,83 @@ namespace PortalDGC.DataAccess.UnitOfWork
 {
     public class UnitOfWork : IUnitOfWork
     {
-        public IPostulanteRepository Postulantes => throw new NotImplementedException();
-
-        public ILlamadoRepository Llamados => throw new NotImplementedException();
-
-        public IDepartamentoRepository Departamentos => throw new NotImplementedException();
-
-        public IInscripcionRepository Inscripciones => throw new NotImplementedException();
-
-        public IAutodefinicionLeyRepository AutodefinicionesLey => throw new NotImplementedException();
-
-        public IRequisitoExcluyenteRepository RequisitosExcluyentes => throw new NotImplementedException();
-
-        public IRequisitoPostulanteRepository RequisitosPostulante => throw new NotImplementedException();
-
-        public IItemPuntuableRepository ItemsPuntuables => throw new NotImplementedException();
-
-        public IMeritoPostulanteRepository MeritosPostulante => throw new NotImplementedException();
-
-        public IApoyoNecesarioRepository ApoyosNecesarios => throw new NotImplementedException();
-
-        public IApoyoSolicitadoRepository ApoyosSolicitados => throw new NotImplementedException();
-
-        public IConstanciaRepository Constancias => throw new NotImplementedException();
-
-        public Task BeginTransactionAsync()
+        private readonly ApplicationDbContext _context;
+        private IDbContextTransaction? _transaction;
+        public UnitOfWork(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
-        }
+            _context = context;
 
-        public Task CommitTransactionAsync()
+            Postulantes = new PostulanteRepository(_context);
+            Llamados = new LlamadoRepository(_context);
+            Departamentos = new DepartamentoRepository(_context);
+            Inscripciones = new InscripcionRepository(_context);
+            AutodefinicionesLey = new AutodefinicionLeyRepository(_context);
+            RequisitosExcluyentes = new RequisitoExcluyenteRepository(_context);
+            RequisitosPostulante = new RequisitoPostulanteRepository(_context);
+            ItemsPuntuables = new ItemPuntuableRepository(_context);
+            MeritosPostulante = new MeritoPostulanteRepository(_context);
+            ApoyosNecesarios = new ApoyoNecesarioRepository(_context);
+            ApoyosSolicitados = new ApoyoSolicitadoRepository(_context);
+            Constancias = new ConstanciaRepository(_context);
+        }
+        public IPostulanteRepository Postulantes { get; private set; }
+        public ILlamadoRepository Llamados { get; private set; }
+        public IDepartamentoRepository Departamentos { get; private set; }
+        public IInscripcionRepository Inscripciones { get; private set; }
+        public IAutodefinicionLeyRepository AutodefinicionesLey { get; private set; }
+        public IRequisitoExcluyenteRepository RequisitosExcluyentes { get; private set; }
+        public IRequisitoPostulanteRepository RequisitosPostulante { get; private set; }
+        public IItemPuntuableRepository ItemsPuntuables { get; private set; }
+        public IMeritoPostulanteRepository MeritosPostulante { get; private set; }
+        public IApoyoNecesarioRepository ApoyosNecesarios { get; private set; }
+        public IApoyoSolicitadoRepository ApoyosSolicitados { get; private set; }
+        public IConstanciaRepository Constancias { get; private set; }
+
+        public async Task BeginTransactionAsync()
         {
-            throw new NotImplementedException();
+            _transaction = await _context.Database.BeginTransactionAsync();
         }
-
+        public async Task CommitTransactionAsync()
+        {
+            try
+            {
+                await _context.SaveChangesAsync();
+                if (_transaction != null)
+                {
+                    await _transaction.CommitAsync();
+                }
+            }
+            catch
+            {
+                await RollbackTransactionAsync();
+                throw;
+            }
+            finally
+            {
+                if (_transaction != null)
+                {
+                    await _transaction.DisposeAsync();
+                    _transaction = null;
+                }
+            }
+        }
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _transaction?.Dispose();
+            _context.Dispose();
         }
-
-        public Task RollbackTransactionAsync()
+        public async Task RollbackTransactionAsync()
         {
-            throw new NotImplementedException();
+            if (_transaction != null)
+            {
+                await _transaction.RollbackAsync();
+                await _transaction.DisposeAsync();
+                _transaction = null;
+            }
         }
-
-        public Task<int> SaveChangesAsync()
+        public async Task<int> SaveChangesAsync()
         {
-            throw new NotImplementedException();
+            return await _context.SaveChangesAsync();
         }
     }
 }
