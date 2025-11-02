2155fc2d19749e0ff36d0ff63bd35ab73b91b025
Gabriel Lutz <gabriel_lutz@hotmail.com>
2025-11-02 16:32:25 -0300

    feat(data): implementa repos de apoyos, constancias y autodefinición

---
+++ PortalDGC/src/PortalDGC.DataAccess/Repositories/AutodefinicionLeyRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortalDGC.DataAccess.Data;
+using PortalDGC.DataAccess.Interfaces;
+using PortalDGC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDGC.DataAccess.Repositories
+{
+    public class AutodefinicionLeyRepository : Repository<AutodefinicionLey>, IAutodefinicionLeyRepository
+    {
+        public AutodefinicionLeyRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+        public async Task<AutodefinicionLey?> GetByInscripcionIdAsync(int inscripcionId)
+        {
+            return await _dbSet
+                .FirstOrDefaultAsync(a => a.InscripcionId == inscripcionId);
+        }
+        public async Task<AutodefinicionLey> CreateOrUpdateAsync(AutodefinicionLey autodefinicion)
+        {
+            var existing = await GetByInscripcionIdAsync(autodefinicion.InscripcionId);
+
+            if (existing != null)
+            {
+                existing.EsAfrodescendiente = autodefinicion.EsAfrodescendiente;
+                existing.EsTrans = autodefinicion.EsTrans;
+                existing.TieneDiscapacidad = autodefinicion.TieneDiscapacidad;
+                return existing;
+            }
+
+            await _dbSet.AddAsync(autodefinicion);
+            return autodefinicion;
+        }
+    }
+}
---
+++ PortalDGC/src/PortalDGC.DataAccess/Repositories/ConstanciaRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortalDGC.DataAccess.Data;
+using PortalDGC.DataAccess.Interfaces;
+using PortalDGC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDGC.DataAccess.Repositories
+{
+    public class ConstanciaRepository : Repository<Constancia>, IConstanciaRepository
+    {
+        public ConstanciaRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+        public async Task<IEnumerable<Constancia>> GetByPostulanteIdAsync(int postulanteId)
+        {
+            return await _dbSet
+                .Where(c => c.PostulanteId == postulanteId)
+                .OrderByDescending(c => c.FechaSubida)
+                .ToListAsync();
+        }
+        public async Task<Constancia> SubirConstanciaAsync(Constancia constancia)
+        {
+            await _dbSet.AddAsync(constancia);
+            return constancia;
+        }
+        public override async Task<Constancia?> GetByIdAsync(int constanciaId)
+        {
+            return await _dbSet
+                .FirstOrDefaultAsync(c => c.Id == constanciaId);
+        }
+        public async Task<bool> ValidarConstanciaAsync(int constanciaId)
+        {
+            var constancia = await GetByIdAsync(constanciaId);
+            if (constancia != null)
+            {
+                constancia.Validado = true;
+                return true;
+            }
+            return false;
+        }
+    }
+}
