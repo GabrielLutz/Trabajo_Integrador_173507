516be9f07bce69581db18c03070f704eabb0aac8
Gabriel Lutz <gabriel_lutz@hotmail.com>
2025-11-10 12:42:04 -0300

    feat(inscripcion): implementa formulario multipaso (Stepper) con validaciones reactivas y envío al backend

--- portal-dgc-frontend/src/app/features/inscripcion/inscripcion-routing.module.ts
+++ portal-dgc-frontend/src/app/features/inscripcion/inscripcion-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioInscripcionComponent } from './components/formulario-inscripcion/formulario-inscripcion.component';
+import { DetalleInscripcionComponent } from './components/detalle-inscripcion/detalle-inscripcion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'nuevo',
+    component: FormularioInscripcionComponent
+  },
+  {
+    path: ':id',
+    component: DetalleInscripcionComponent
+  },
+  {
+    path: '',
+    redirectTo: 'nuevo',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
