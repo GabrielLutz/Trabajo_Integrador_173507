9c65a4a2f5e6ee48ebb96ccdbd8affa1aebfedc2
Gabriel Lutz <gabriel_lutz@hotmail.com>
2025-11-09 22:44:23 -0300

    feat(db/migration): ajusta esquema para módulo Tribunal (Pruebas, Evaluaciones, Ordenamientos)

--- PortalDGC/src/PortalDGC.DataAccess/Migrations/20251110013728_TribunalAdjustments.Designer.cs
+++ PortalDGC/src/PortalDGC.DataAccess/Migrations/20251110013728_TribunalAdjustments.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PortalDGC.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using PortalDGC.DataAccess.Data;
 namespace PortalDGC.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251110013728_TribunalAdjustments")]
+    partial class TribunalAdjustments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
