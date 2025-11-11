2f3a729bb76d3b16f81607ea42f4144a77623bd2
Gabriel Lutz <gabriel_lutz@hotmail.com>
2025-11-11 11:01:52 -0300

    chore(frontend): configura estilos globales y build (styles.scss, angular.json, proxy, tsconfig, index/main)

--- portal-dgc-frontend/src/app/features/postulante/components/mis-inscripciones/mis-inscripciones.component.spec.ts
+++ portal-dgc-frontend/src/app/features/postulante/components/mis-inscripciones/mis-inscripciones.component.spec.ts
@@ -1,14 +1,32 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { SharedModule } from '../../../../shared/shared.module';
+import { InscripcionService } from '../../../../core/services/inscripcion.service';
 import { MisInscripcionesComponent } from './mis-inscripciones.component';
 
 describe('MisInscripcionesComponent', () => {
   let component: MisInscripcionesComponent;
   let fixture: ComponentFixture<MisInscripcionesComponent>;
+  const inscripcionesResponse = {
+    success: true,
+    message: '',
+    data: [],
+    errors: []
+  };
 
   beforeEach(async () => {
+    const inscripcionServiceMock = {
+      obtenerInscripcionesPorPostulante: jasmine
+        .createSpy('obtenerInscripcionesPorPostulante')
+        .and.returnValue(of(inscripcionesResponse))
+    };
+
     await TestBed.configureTestingModule({
       declarations: [MisInscripcionesComponent],
+      imports: [SharedModule, RouterTestingModule],
+      providers: [{ provide: InscripcionService, useValue: inscripcionServiceMock }],
       schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
